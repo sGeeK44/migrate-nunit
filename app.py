90096699efc7db535945ba27660885a4bec42c5e
François Perin <f.perin@smartway.ai>
2024-10-21 15:32:38 +0200

    feat: Add args to specify folder

--- app.py
+++ app.py
@@ -1,77 +1,57 @@
-import openai
-from dotenv import load_dotenv
+import argparse
 import os
-
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
+from dotenv import load_dotenv
 from openai import OpenAI
 
-if __name__ == "__main__":    
-    try:        
+
+load_dotenv()
+
+
+def get_files(directory):
+    cs_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".cs"):
+                cs_files.append(os.path.join(root, file))
+    return cs_files
+
+
+def read_file(file_path: str):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
+
+
+def write_file(file_path: str, content:str):
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+def convert(nunit: str) -> str:
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "Convert NUnit test to XUnit test. Response should constains only code without any additionnal comment of your work inside and no markdown format. Dont forget to add new line at the end of file to be compliant with git."},
+            {
+                "role": "user",
+                "content": nunit}
+        ]
+    )
+    return completion.choices[0].message.content
+
+if __name__ == "__main__":    # Set up argument parsing
+    parser = argparse.ArgumentParser(description="Convert NUnit tests style to XUnit style.")
+    parser.add_argument("dir", help="Root UnitTest directory path.")
+    args = parser.parse_args()
+
+    try:
+        print("Starting conversions")
         client = OpenAI()
-        completion = client.chat.completions.create(
-            model="gpt-4o",
-            messages=[
-                {"role": "system", "content": "Convert NUnit test to XUnit test. Response should constains only code without any additionnal comment of your work inside and no markdown format."},
-                {
-                    "role": "user",
-                    "content": """
-using Newtonsoft.Json;
-using NFluent;
-using NUnit.Framework;
-using Zg.Lib.Serialization.Json;
-
-namespace Zg.Api.UnitTests
-{
-    [TestFixture]
-    public class ApiResponseTest
-    {
-        public JsonSerializer<APIResponse> JsonSerializer { get; private set; }
-
-        [SetUp]
-        public void SetUp()
-        {
-            JsonSerializer = new JsonSerializer<APIResponse>();
-
-        }
-
-        [TestCase("A")]
-        [TestCase("B")]
-        public void DeserializeFromApiStream(string x)
-        {
-            var json = "{\"error\":{\"code\":\""+ x + "\",\"message\":\"B\"},\"type\":\"C\",\"value\":\"D\",\"commands\":[{\"id\":1,\"name\":\"E\",\"arguments\":\"F\"}]}";
-
-            var result = JsonSerializer.Deserialize(json);
-
-
-            Check.That(result.Error.Code).IsEqualTo("A");
-            Check.That(result.Error.Message).IsEqualTo("B");
-            Check.That(result.Type).IsEqualTo("C");
-            Check.That(result.Value).IsEqualTo("D");
-            Check.That(result.Commands.Count).IsEqualTo(1);
-            Check.That(result.Commands[0].Id).IsEqualTo(1);
-            Check.That(result.Commands[0].Name).IsEqualTo("E");
-            Check.That(result.Commands[0].Arguments).IsEqualTo("F");
-        }
-
-        [Test]
-        public void ApiResultV4()
-        {
-            var json =
-                "{\"created\": [90012, 90210, 90211, 90212], \"accepted\": [90213, 91726, 91741, 97958], \"errors\": []}";
-
-            var results = JsonSerializer.Deserialize(json);
-
-            Check.That(results.Error.Code).IsEqualTo("A");
-            Check.That(results.Error.Message).IsEqualTo("B");
-        }
-    }
-}
-"""
-                }
-            ]
-        )        
-        print(completion.choices[0].message.content)
+        for cs_file in get_files(args.dir):
+            print(f"Convert {cs_file}")
+            nunit = read_file(cs_file)
+            xunit = convert(nunit)
+            write_file(cs_file, xunit)
+            print(f"Done.")
     except Exception as e:
-        print(f"Error interacting with OpenAI: {e}")
+        print(f"Fatal: {e}")
